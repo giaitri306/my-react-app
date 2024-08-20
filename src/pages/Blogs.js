f3a6c77fa22ebcff62b87bbe5db4b416f6b77ffd
giaitri306 <giaitri.zvni@gmail.com>
2024-08-20 17:44:58 +0700

    first commit

---
+++ src/pages/Blogs.js
@@ -0,0 +1,27 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+const Blogs = () => {
+    return (
+        <>
+            <Container>
+                <Row>
+                    <h1 className="text-center">Blogs Content</h1>
+                </Row>
+                <Row>
+                    <Col>1 of 1</Col>
+                    <p>feafewa</p>
+                </Row>
+            </Container>
+            
+           
+        </>
+        
+    );
+  
+};
+  
+  
+export default Blogs;
